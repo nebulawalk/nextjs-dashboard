da468a960bdf2bf1ef012e9e4154612b94fd8d02
neb <25150296+nebulawalk@users.noreply.github.com>
2024-12-12 16:32:36 +0900

    ESLintの設定ファイルを追加し、パッケージにlintスクリプトを追加。データベース操作のエラーハンドリングを強化し、ログインフォームのコードスタイルを整形。

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,6 +31,8 @@ export async function createInvoice(formData: FormData) {
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
 	} catch (error) {
+		console.log(error);
+
 		return {
 			message: "Database Error: Failed to Create Invoice.",
 		};
@@ -58,6 +60,8 @@ export async function updateInvoice(id: string, formData: FormData) {
             WHERE id = ${id}
         `;
 	} catch (error) {
+		console.log(error);
+
 		return { message: "Database Error: Failed to Update Invoice." };
 	}
 
@@ -66,13 +70,13 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-	throw new Error("Failed to Delete Invoice");
-
 	try {
 		await sql`DELETE FROM invoices WHERE id = ${id}`;
 		revalidatePath("/dashboard/invoices");
 		return { message: "Deleted Invoice." };
 	} catch (error) {
+		console.log(error);
+
 		return { message: "Database Error: Failed to Delete Invoice." };
 	}
 }
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,7 +2,7 @@ import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
-import type { LatestInvoice } from "@/app/lib/definitions";
+// import type { LatestInvoice } from "@/app/lib/definitions";
 import { fetchLatestInvoices } from "@/app/lib/data";
 
 export default async function LatestInvoices() {
